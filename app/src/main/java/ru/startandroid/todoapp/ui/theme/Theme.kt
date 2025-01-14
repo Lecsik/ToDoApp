4f49377275f6926163c10a701375f2a98900e71a
Lecsik <naleksandrovav@gmail.com>
2025-01-14 17:53:50 +0300

    Обновленны зависимости в gradle

--- app/src/main/java/ru/startandroid/todoapp/ui/theme/Theme.kt
+++ app/src/main/java/ru/startandroid/todoapp/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -54,7 +53,6 @@ fun MyTheme(
     val view = LocalView.current
     SideEffect {
         val window = (view.context as? Activity)?.window ?: return@SideEffect
-        window.statusBarColor = colorScheme.primaryContainer.toArgb()
         WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
 
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -13,11 +13,11 @@ kotlin.compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM
 
 dependencies {
     implementation("joda-time:joda-time:2.13.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("org.xerial:sqlite-jdbc:3.47.0.0")
-    implementation("io.ktor:ktor-server-cors:3.0.1")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.0.1")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.0.1")
-    implementation("io.ktor:ktor-server-netty-jvm:3.0.1")
-    implementation("ch.qos.logback:logback-classic:1.5.12")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("org.xerial:sqlite-jdbc:3.47.2.0")
+    implementation("io.ktor:ktor-server-cors:3.0.3")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.0.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.0.3")
+    implementation("io.ktor:ktor-server-netty-jvm:3.0.3")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
 }
