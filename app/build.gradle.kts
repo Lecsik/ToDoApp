895486cf102dd287da0864b314582c3553951a3d
Lecsik <naleksandrovav@gmail.com>
2025-09-28 23:01:05 +0300

    Обновлены файлы Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -14,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.startandroid.todoapp"
-        minSdk = 21
+        minSdk = 23
         //noinspection EditedTargetSdkVersion
         targetSdk = 36
         versionCode = 1
@@ -43,50 +45,52 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
 }
 
 kotlin {
     jvmToolchain(17)
+
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("joda-time:joda-time:2.14.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
-    implementation("androidx.room:room-runtime:2.7.2")
-    ksp("androidx.room:room-compiler:2.7.2")
-    implementation("androidx.room:room-ktx:2.7.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.room:room-runtime:2.8.1")
+    ksp("androidx.room:room-compiler:2.8.1")
+    implementation("androidx.room:room-ktx:2.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
     implementation("org.kodein.di:kodein-di:7.26.1")
     implementation("org.kodein.di:kodein-di-framework-android-x-viewmodel:7.26.1")
 
-    implementation("androidx.compose.ui:ui:1.8.3")
-    implementation("androidx.compose.runtime:runtime:1.8.3")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.06.01"))
+    implementation("androidx.compose.ui:ui:1.9.2")
+    implementation("androidx.compose.runtime:runtime:1.9.2")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation(platform("androidx.compose:compose-bom:2025.09.01"))
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
-    implementation("androidx.compose.runtime:runtime-livedata:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.2")
 
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.2")
-    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.4.0")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0")
+    implementation("androidx.navigation:navigation-compose:2.9.5")
 
-    implementation("androidx.work:work-runtime:2.10.2")
+    implementation("androidx.work:work-runtime:2.10.5")
 }
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -13,13 +13,13 @@ kotlin.compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM
 
 dependencies {
     implementation("joda-time:joda-time:2.14.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
-    implementation("org.xerial:sqlite-jdbc:3.50.1.0")
-    implementation("io.ktor:ktor-server-cors:3.2.0")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.2.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.2.0")
-    implementation("io.ktor:ktor-server-netty-jvm:3.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.xerial:sqlite-jdbc:3.50.3.0")
+    implementation("io.ktor:ktor-server-cors:3.3.0")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.3.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.3.0")
+    implementation("io.ktor:ktor-server-netty-jvm:3.3.0")
     implementation("ch.qos.logback:logback-classic:1.5.18")
-    implementation("io.ktor:ktor-server-auth:3.2.0")
+    implementation("io.ktor:ktor-server-auth:3.3.0")
 
 }
